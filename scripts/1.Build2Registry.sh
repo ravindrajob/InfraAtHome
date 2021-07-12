0920abe012ab30eecff9c0f2cd6c04a60d0f8990
Ravindra JOB <85297393+ravindrajob@users.noreply.github.com>
2021-07-12 14:51:23 +0200

    Update 1.Build2Registry.sh

--- scripts/1.Build2Registry.sh
+++ scripts/1.Build2Registry.sh
@@ -2,9 +2,28 @@
 #     Build images      #
 #########################
 
-###################
+########################
+# Update Docker Config #
+########################
+
+cat > /etc/docker/daemon.json << EOF
+{
+  "log-driver": "loki",
+  "log-opts": {
+    "loki-url": "http://127.0.0.1:3100/loki/api/v1/push",
+    "max-size": "50m",
+    "max-file": "10"
+  },
+  "registry-mirrors": ["yourregistryIP:5000"]
+}
+EOF
+
+#################
 ##### API-GW ####
+#################
+
 versionapigw="v0.02"
+
 #---> BUILD
 docker build -t apigw:$versionapigw /home/ravindra/docker/api-gw/
 # docker rmi api-gw:v0.01
